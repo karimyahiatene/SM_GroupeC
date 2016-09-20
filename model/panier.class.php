488bca665d39a78194a256f2134dc47b17c5ba4a
Jonathan SCHOTS ALSUGUREN <jonathan.schotsalsuguren@greta-sud-aquitaine.fr>
2016-09-20 10:49:46 +0200

    Ajout calcul du montant du Panier

--- model/panier.class.php
+++ model/panier.class.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once("vue/fonctions.php");
 
 class Panier{
@@ -27,20 +27,25 @@ class Panier{
                 $donnee['quantite'] = 1;
                 $this->liste[$donnee['codeA']] = $donnee;
         }
-    $this->nombreArticles += 1; 
+	$this->prixTotal += $donnee['prixhtA'];
+    $this->nombreArticles += 1;
     }
 
     public function del_panier($codeArticle){
         if (!empty ($this->liste)){
+			$this->prixTotal = $this->liste[$codeArticle]['prixhtA'];
             $this->liste[$codeArticle]['quantite'] -= 1;
             if ($this->liste[$codeArticle]['quantite'] == 0)
                 unset($this->liste[$codeArticle]);
         }
-    $this->nombreArticles -= 1; 
+    $this->nombreArticles -= 1;
     }
 
     public function getPanier(){
         return $this->liste;
     }
-}
+
+	public function getTotal(){
+		return $this->prixTotal;
+	}
 ?>
