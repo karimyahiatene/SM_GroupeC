b3b0422fb2e857b9e007f6efff15a20b213440e0
Jonathan SCHOTS ALSUGUREN <jonathan.schotsalsuguren@greta-sud-aquitaine.fr>
2016-09-20 11:32:51 +0200

    Fix : Panier.class.php

--- model/panier.class.php
+++ model/panier.class.php
@@ -1,20 +1,16 @@
 <?php
 require_once("vue/fonctions.php");
-
 class Panier{
     private $liste = array();
     private $nombreArticles = 0;
     private $prixTotal = 0.00;
-
     public function add_panier($codeArticle){
         global $pdo;
-
         $sql = "SELECT * FROM tb_article WHERE codeA = :codeArticle";
         $pdo_select = $pdo->prepare($sql);
         $pdo_select->bindValue(':codeArticle', $codeArticle);
         $pdo_select->execute();
         $donnee = $pdo_select->fetch();
-
         if(!empty($this->liste)){
             if (array_key_exists($donnee['codeA'], $this->liste)){
                 $this->liste[$donnee['codeA']]['quantite'] += 1;
@@ -27,12 +23,9 @@ class Panier{
                 $donnee['quantite'] = 1;
                 $this->liste[$donnee['codeA']] = $donnee;
         }
-    $this->nombreArticles += 1; 
-        }
 	$this->prixTotal += $donnee['prixhtA'];
     $this->nombreArticles += 1;
     }
-
     public function del_panier($codeArticle){
         if (!empty ($this->liste)){
 			$this->prixTotal -= $this->liste[$codeArticle]['prixhtA'];
@@ -42,11 +35,9 @@ class Panier{
         }
     $this->nombreArticles -= 1;
     }
-
     public function getPanier(){
         return $this->liste;
     }
-
 	public function getTotal(){
 		return $this->prixTotal;
 	}
