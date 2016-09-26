e4bcb096d12b4a762fdedf8244b82d19dba16915
Jonathan SCHOTS ALSUGUREN <jonathan.schotsalsuguren@greta-sud-aquitaine.fr>
2016-09-26 09:53:36 +0200

    Accueil message

--- vue/accueil.view.php
+++ vue/accueil.view.php
@@ -1,9 +1,13 @@
 <?php $titrePage = 'Accueil'; ?>
 
 <?php ob_start(); ?>
+<div id="cadreProduits">
+    <h1>Bienvenue</h1>
+    <p>
+        <br />
+        Faites vos courses sur notre boutique en ligne et choisissez quand vous souhaitez les récupérer avec notre Drive! 
+    </p>
+    </div>
+    <?php $contenuPage = ob_get_clean(); ?>
 
-<h1>Bienvenue</h1>
-
-<?php $contenuPage = ob_get_clean(); ?>
-
-<?php require 'gabarit.view.php'; ?>
+    <?php require 'gabarit.view.php'; ?>
