6d16073b809f277af176db2f4f92c58258f1d0da
Jonathan SCHOTS ALSUGUREN <jonathan.schotsalsuguren@greta-sud-aquitaine.fr>
2016-09-26 10:07:59 +0200

    .htaccess 404

--- vue/erreur.view.php
+++ vue/erreur.view.php
@@ -1,9 +1,11 @@
 <?php $titrePage = 'Erreur'; ?>
 <?php ob_start(); ?>
+<div id="cadreProduits">
 <?php
 if ($codeErreur == 1)
     echo "<h1>La page demandé n'existe pas.</h1>";
 ?>
+</div>
 <?php $contenuPage = ob_get_clean(); ?>
 
 <?php require 'gabarit.view.php'; ?>
